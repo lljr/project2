a2d4922d84aece70689222a0081789adf43b3742
jon rostran <rostranjj@gmail.com>
2019-06-19 16:28:17 -0600

    start implementing user display name setup

---
+++ static/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // create a username if none set
+  if (!localStorage.getItem('username')) {
+    localStorage.setItem('username', "")
+  }
+
+  // if username is empty
+  // hide channel item in navbar menu
+  // else, show it as an option
+
+
+});
