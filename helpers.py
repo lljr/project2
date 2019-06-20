64bbdc56e5cbed66e0378371d05f282cfae2d84f
jon rostran <rostranjj@gmail.com>
2019-06-20 13:03:38 -0600

    create decorator for required login routes

---
+++ helpers.py
@@ -0,0 +1,14 @@
+from flask import session, redirect, url_for
+
+from functools import wraps
+
+
+def login_required(f):
+    """Decorate routes to require user login."""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("username") is None:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+
+    return decorated_function
