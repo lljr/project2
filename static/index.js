67b229b9f6e6ace2294cbf8e948d16be009c5262
jon rostran <rostranjj@gmail.com>
2019-06-20 16:56:41 -0600

    remove some comments

--- static/index.js
+++ static/index.js
@@ -2,12 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // create a username if none set
   if (!localStorage.getItem('username')) {
-    localStorage.setItem('username', "")
-  }
+    localStorage.setItem('username', () => {
 
-  // if username is empty
-  // hide channel item in navbar menu
-  // else, show it as an option
-  // maybe i could use disabled attribute???
+
+      // if username is empty
+      document.querySelector("#form").onsubmit = () => {
+
+
+      }
+      // hide channel item in navbar menu
+      // else, show it as an option
+      // maybe i could use disabled attribute???
+
+    })
+  }
 
 });
