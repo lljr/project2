d95ce602bc839200f3c3c8e91d4edb8c818709f6
jon rostran <rostranjj@gmail.com>
2019-06-21 15:01:28 -0600

    add a decorator that checks for a query arg on url

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-from flask import session, redirect, url_for
+from flask import session, redirect, url_for, request
 
 from functools import wraps
 
@@ -7,8 +7,24 @@ def login_required(f):
     """Decorate routes to require user login."""
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        # TODO maybe I could use request.args.get("user") instead of working
+        # with session...
+        # OR I could do both
+        # like if session.get("username") is None or request.args.get("username") is None
         if session.get("username") is None:
-            return redirect(url_for('login'))
+            return redirect(url_for('adduser'))
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def name_required(f):
+    """Decorate route to require a display name."""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if request.args.get("name") is None:
+            return redirect('adduser')
+
         return f(*args, **kwargs)
 
     return decorated_function
