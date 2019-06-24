bac8df7fece465116310cc953ab9cae543b89bc3
jon rostran <rostranjj@gmail.com>
2019-06-24 12:55:20 -0600

    del experimental helper function

--- helpers.py
+++ helpers.py
@@ -7,24 +7,8 @@ def login_required(f):
     """Decorate routes to require user login."""
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        # TODO maybe I could use request.args.get("user") instead of working
-        # with session...
-        # OR I could do both
-        # like if session.get("username") is None or request.args.get("username") is None
         if session.get("username") is None:
             return redirect(url_for('adduser'))
-        return f(*args, **kwargs)
-
-    return decorated_function
-
-
-def name_required(f):
-    """Decorate route to require a display name."""
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if request.args.get("name") is None:
-            return redirect('adduser')
-
         return f(*args, **kwargs)
 
     return decorated_function
