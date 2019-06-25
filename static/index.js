62e8d7110388dce67e7f339d0fd7769670b2544b
jon rostran <rostranjj@gmail.com>
2019-06-25 16:32:58 -0600

    trying to build channel creating AJAX request

--- static/index.js
+++ static/index.js
@@ -1,20 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  // create a username if none set
-  if (!localStorage.getItem('username')) {
-    localStorage.setItem('username', () => {
+  // if (!localStorage.getItem('username')) {
+  //   localStorage.setItem('username', () => {
+  //   })
+  // }
+  // Send request to create a channel
+  document.querySelector("#channelform").onsubmit = () => {
 
+    // Initialize request
+    const request = new XMLHttpRequest();
+    const channelName = document.querySelector("#channelname").value;
+    request.open('POST',"/createchannel");
 
-      // if username is empty
-      document.querySelector("#form").onsubmit = () => {
-
+    // Callback function for when request completes
+    request.onload = () => {
+      // Extract JSON from response
+      const data = JSON.parse(request.responseText);
+      console.log(data)
+      console.log(`data success ${data.success}`)
+      console.log(`request success ${data.success}`)
+      // Update the result div
+      if (request.success) {
+        // data.channel & data.message
+        document.querySelector('#message').innerHTML = `${data.message}`;
+        console.log(data.message);
 
       }
-      // hide channel item in navbar menu
-      // else, show it as an option
-      // maybe i could use disabled attribute???
+      else {
+        document.querySelector('#message').innerHTML = `There was an error`;
+        console.log(data.message);
 
-    })
+      }
+    };
+
+    const data = new FormData();
+    data.append('channelname', channelName);
+
+    // Send request
+    request.send(data);
+    return false;
+
   }
 
 });
