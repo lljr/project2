99a56ee6ad7dff83aa493b95ad02cc0620b92d56
jon rostran <rostranjj@gmail.com>
2019-06-26 16:33:35 -0600

    remove debug functions (console.log), fix conditional statement

--- static/index.js
+++ static/index.js
@@ -12,24 +12,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const channelName = document.querySelector("#channelname").value;
     request.open('POST',"/createchannel");
 
-    // Callback function for when request completes
     request.onload = () => {
-      // Extract JSON from response
       const data = JSON.parse(request.responseText);
-      console.log(data)
-      console.log(`data success ${data.success}`)
-      console.log(`request success ${data.success}`)
-      // Update the result div
-      if (request.success) {
-        // data.channel & data.message
-        document.querySelector('#message').innerHTML = `${data.message}`;
-        console.log(data.message);
 
+      if (data.success) {
+        document.querySelector('#message').innerHTML = `${data.message}`;
+        updateLiveChannelsList();
       }
       else {
         document.querySelector('#message').innerHTML = `There was an error`;
-        console.log(data.message);
-
       }
     };
 
