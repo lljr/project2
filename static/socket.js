55054eae56c07db3dacfacccd6c3d4d5eded69d5
jon rostran <rostranjj@gmail.com>
2019-07-18 11:02:12 -0600

    delete file merge into index.json

--- static/socket.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMcontentloaded", () => {
-  var socket = io.connect(location.protocol + "//" + document.domain + ":" + location.port);
-
-  socket.on("connect", () => {
-
-  });
-
-})
