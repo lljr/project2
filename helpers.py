8b93d5340dbfb778472dafb099fbc6c0ff87643f
jon rostran <rostranjj@gmail.com>
2019-07-29 15:11:12 -0600

    bring back fun decorator for websocket func handlers

--- helpers.py
+++ helpers.py
@@ -1,1 +1,13 @@
+import functools
+from flask import session
+from flask_socketio import disconnect
 
+
+def authenticated_only(f):
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        if not session.username:
+            disconnect()
+        else:
+            return f(*args, **kwargs)
+    return wrapped
