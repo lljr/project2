6370fd2efdad1e8ad08d65ec779376c8793537e7
jon rostran <rostranjj@gmail.com>
2019-07-31 16:42:11 -0600

    move function definitions outside of DOMContentloaded scope

--- static/index.js
+++ static/index.js
@@ -64,113 +64,123 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  // TODO When logging out, clear localStorage
-  function updateChannelsList(channelName, ul, updateStorage="no") {
-
-    // TODO The Join button only needs to be created once?
-    const joinButton = document.createElement("button");
-    setUpJoinChannelButton(joinButton)
-
-    if (updateStorage === "yes")
-      localStorage.pushArrayItem('channels', channelName);
-
-    const li = document.createElement("li");
-    setUpChannelListElement(li, joinButton, channelName);
-
-    // Set the card class here to prevent styling to appear on page
-    // and take space on the screen when there are no chat rooms alive
-    if (!ul.parentNode.getAttribute("class"))
-      ul.parentNode.setAttribute("class", "card");
-
-    ul.append(li);
-  }
-
-  function clearOutListData(ul) {
-    // Clears out existing list data `li` on a `ul` parent node
-    while (ul.firstChild) {
-      ul.removeChild(ul.firstChild);
-    }
-  }
-
-  function setUpJoinChannelButton(button) {
-    button.innerHTML = "Join";
-    button.setAttribute("class", "btn btn-primary ml-auto");
-    button.setAttribute("type", "button");
-  }
-
-  function setUpChannelListElement(li, button, channelName) {
-    li.setAttribute("class", "list-group-item d-flex align-items-center");
-    li.setAttribute("id", `channel-item-${channelName}`);
-    li.setAttribute("data-channel", `${channelName}`);
-    li.append(channelName, button);
-  }
-
-  function enterChat(name, clickedEl) {
-
-    // Make sure an `li` element gets clicked
-    // TODO make sure `button` element gets clicked
-    if (clickedEl.nodeName == "LI") {
-      const channelName = clickedEl.dataset.channel
-      // Send request to chat room view
-      const request = new XMLHttpRequest();
-
-      request.open("GET", `/channel/${channelName}`);
-
-      request.onload = function() {
-        // TODO open socket to room
-        socket.emit('join', channelName);
-      }
-
-      request.send();
-    }
-
-  }
-
-  // ======= Storage helper functions start here =======
-  // https://stackoverflow.com/a/23516713
-  Storage.prototype.getArray = function(arrayName) {
-    var thisArray = [];
-    var fetchArrayObject = this.getItem(arrayName);
-    if (typeof fetchArrayObject !== 'undefined') {
-      if (fetchArrayObject !== null) { thisArray = JSON.parse(fetchArrayObject); }
-    }
-    return thisArray;
-  }
-
-  Storage.prototype.pushArrayItem = function(arrayName,arrayItem) {
-    var existingArray = this.getArray(arrayName);
-    existingArray.push(arrayItem);
-    this.setItem(arrayName,JSON.stringify(existingArray));
-  }
-
-  Storage.prototype.popArrayItem = function(arrayName) {
-    var arrayItem = {};
-    var existingArray = this.getArray(arrayName);
-    if (existingArray.length > 0) {
-      arrayItem = existingArray.pop();
-      this.setItem(arrayName,JSON.stringify(existingArray));
-    }
-    return arrayItem;
-  }
-
-  Storage.prototype.shiftArrayItem = function(arrayName) {
-    var arrayItem = {};
-    var existingArray = this.getArray(arrayName);
-    if (existingArray.length > 0) {
-      arrayItem = existingArray.shift();
-      this.setItem(arrayName,JSON.stringify(existingArray));
-    }
-    return arrayItem;
-  }
-
-  Storage.prototype.unshiftArrayItem = function(arrayName,arrayItem) {
-    var existingArray = this.getArray(arrayName);
-    existingArray.unshift(arrayItem);
-    this.setItem(arrayName,JSON.stringify(existingArray));
-  }
-
-  Storage.prototype.deleteArray = function(arrayName) {
-    this.removeItem(arrayName);
-  }
-  // ======= Storage helper functions end here =======
 });
+
+function setUsernameForm(mainRow, socket) {
+  // TODO needs work....
+  const form = document.querySelector("#user-form");
+
+  form.onsubmit = () => {
+
+    const username = input.value;
+
+
+    input.value = '';
+
+    return false;
+  };
+}
+
+// TODO When logging out, clear localStorage
+function updateChannelsList(channelName, ul, updateStorage="no") {
+
+  // TODO The Join button only needs to be created once?
+  const joinButton = document.createElement("button");
+  setUpJoinChannelButton(joinButton)
+
+  if (updateStorage === "yes")
+    localStorage.pushArrayItem('channels', channelName);
+
+  const li = document.createElement("li");
+  setUpChannelListElement(li, joinButton, channelName);
+
+  // Set the card class here to prevent styling to appear on page
+  // and take space on the screen when there are no chat rooms alive
+  if (!ul.parentNode.getAttribute("class"))
+    ul.parentNode.setAttribute("class", "card");
+
+  ul.append(li);
+}
+
+function clearOutListData(ul) {
+  // Clears out existing list data `li` on a `ul` parent node
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+}
+
+function setUpJoinChannelButton(button) {
+  button.innerHTML = "Join";
+  button.setAttribute("class", "btn btn-primary ml-auto");
+  button.setAttribute("type", "button");
+}
+
+function setUpChannelListElement(li, button, channelName) {
+  li.setAttribute("class", "list-group-item d-flex align-items-center");
+  li.setAttribute("id", `channel-item-${channelName}`);
+  li.setAttribute("data-channel", `${channelName}`);
+  li.append(channelName, button);
+}
+
+function joinRoom(event) {
+  const clickedEl = event.target;
+  if (clickedEl.nodeName === "LI") {
+    // TODO check the user joined the channel
+    // TODO switchChatView()
+  } else if (clickedEl.nodeName === "BUTTON") {
+    const li = clickedEl.parentNode;
+    // TODO switchChatView()
+
+    socket.emit('join', {
+      "room": li.dataset.channel
+    });
+
+  }
+}
+// ======= Storage helper functions start here =======
+// https://stackoverflow.com/a/23516713
+Storage.prototype.getArray = function(arrayName) {
+  var thisArray = [];
+  var fetchArrayObject = this.getItem(arrayName);
+  if (typeof fetchArrayObject !== 'undefined') {
+    if (fetchArrayObject !== null) { thisArray = JSON.parse(fetchArrayObject); }
+  }
+  return thisArray;
+}
+
+Storage.prototype.pushArrayItem = function(arrayName,arrayItem) {
+  var existingArray = this.getArray(arrayName);
+  existingArray.push(arrayItem);
+  this.setItem(arrayName,JSON.stringify(existingArray));
+}
+
+Storage.prototype.popArrayItem = function(arrayName) {
+  var arrayItem = {};
+  var existingArray = this.getArray(arrayName);
+  if (existingArray.length > 0) {
+    arrayItem = existingArray.pop();
+    this.setItem(arrayName,JSON.stringify(existingArray));
+  }
+  return arrayItem;
+}
+
+Storage.prototype.shiftArrayItem = function(arrayName) {
+  var arrayItem = {};
+  var existingArray = this.getArray(arrayName);
+  if (existingArray.length > 0) {
+    arrayItem = existingArray.shift();
+    this.setItem(arrayName,JSON.stringify(existingArray));
+  }
+  return arrayItem;
+}
+
+Storage.prototype.unshiftArrayItem = function(arrayName,arrayItem) {
+  var existingArray = this.getArray(arrayName);
+  existingArray.unshift(arrayItem);
+  this.setItem(arrayName,JSON.stringify(existingArray));
+}
+
+Storage.prototype.deleteArray = function(arrayName) {
+  this.removeItem(arrayName);
+}
+// ======= Storage helper functions end here =======
